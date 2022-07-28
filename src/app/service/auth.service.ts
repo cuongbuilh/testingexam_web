aa526d6c1155467f5adb771e038c9dd9c3a2ae46
cuongbv <08thang8@gmail.com>
2022-07-28 15:50:14 +0700

    Conditional Navbar Links

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,14 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   isAuthenticated(): boolean{
     if((sessionStorage.getItem('token') !== null)) return true
     return false
+  }
+
+  canAccess() {
+    if(!this.isAuthenticated()){
+      this.router.navigate(['/login']);
+    }
   }
 }
