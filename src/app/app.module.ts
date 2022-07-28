eab22b02aa8395f15e9fdc97346b2abf19b45245
cuongbv <08thang8@gmail.com>
2022-07-28 14:51:39 +0700

    Updating

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './component/navbar/navbar.component';
+import { NavbarComponent } from './component/dashboard/navbar/navbar.component';
+import { HomeComponent } from './component/pages/home/home.component';
+import { RegisterComponent } from './component/pages/register/register.component';
+import { LoginComponent } from './component/pages/login/login.component';
+import { DashboardComponent } from './component/dashboard/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    RegisterComponent,
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
